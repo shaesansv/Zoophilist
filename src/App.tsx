0392837498e7eb4c27923bda15ae3a7c863ad515
boopathi-raja06 <boopathraja060700@gmail.com>
2025-09-20 10:13:16 +0530

    Clean up: Remove debug components now that routing is working with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ForestAnimations from "./components/ForestAnimations";
 import PrivateRouteAdmin from "./components/PrivateRouteAdmin";
-import DebugRouting from "./components/DebugRouting";
 
 // User Pages
 import Home from "./pages/user/Home";
@@ -22,7 +21,6 @@ import AdminProducts from "./pages/admin/AdminProducts";
 import AdminCustomers from "./pages/admin/AdminCustomers";
 
 import NotFound from "./pages/NotFound";
-import TestPage from "./pages/TestPage";
 
 const queryClient = new QueryClient();
 
@@ -33,7 +31,6 @@ const App = () => (
         <Toaster />
         <Sonner />
         <HashRouter>
-          <DebugRouting />
           <Navbar />
           <ForestAnimations />
           <Routes>
@@ -41,9 +38,6 @@ const App = () => (
             <Route path="/" element={<Home />} />
             <Route path="/order" element={<Order />} />
             <Route path="/contact" element={<Contact />} />
-            
-            {/* Test Route */}
-            <Route path="/test" element={<TestPage />} />
 
             {/* Admin Routes */}
             <Route path="/admin" element={<Navigate to="/admin/login" replace />} />
